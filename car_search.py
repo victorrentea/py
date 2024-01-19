38bae3a374054c22c5875c7d87e0d0f7afca0125
Victor Rentea <victorrentea@gmail.com>
2024-01-19 19:07:09 +0200

    END

--- car_search.py
+++ car_search.py
@@ -1,10 +1,28 @@
-class CarSearchCriteria:
-    def __init__(self, start_year, end_year, make):
-        self.make = make
-        if start_year > end_year:
+from dataclasses import dataclass
+@dataclass(frozen=True)
+class Interval:
+    start: int
+    end: int
+
+    def __post_init__(self):
+        if self.start > self.end:
             raise ValueError("Start year is larger than end year.")
-        self.start_year = start_year
-        self.end_year = end_year
+        #multe = safety: fail fast de cate ori poti, cat mai devreme
+        #putin = mai reusable
+
+    #pot si eu OOP: keep behavior close to data
+    def intersects(self, other):
+        return self.start <= other.end and other.start <= self.end
+
+
+class CarSearchCriteria:
+    def __init__(self, interval: Interval, make):
+        self.make = make
+        # if start_year > end_year:
+        #     raise ValueError("Start year is larger than end year.")
+        # self.start_year = start_year
+        # self.end_year = end_year
+        self.year_interval = interval
 
     def get_start_year(self):
         return self.start_year
@@ -15,15 +33,16 @@ class CarSearchCriteria:
     def get_make(self):
         return self.make
 
+    @property
+    def year_interval(self):
+        return Interval(self.start_year, self.end_year)
+
 
 class CarModel:
-    def __init__(self, make, model, start_year, end_year):
+    def __init__(self, make, model, interval: Interval):
         self.make = make
         self.model = model
-        if start_year > end_year:
-            raise ValueError("Start year is larger than end year.")
-        self.start_year = start_year
-        self.end_year = end_year
+        self.year_interval = interval
 
     def get_end_year(self):
         self.make = 2
@@ -31,6 +50,7 @@ class CarModel:
 
     def get_start_year(self):
         return self.start_year
+
 
     def get_make(self):
         return self.make
@@ -43,37 +63,31 @@ class CarModel:
 
 
 def filter_car_models(criteria: CarSearchCriteria, car_models: list[CarModel]):
-    def years_match(car_model, criteria):
-        return intervals_intersect(
-            criteria.get_start_year(), criteria.get_end_year(),
-            car_model.get_start_year(), car_model.get_end_year())
-
-    matches = [car_model for car_model in car_models if years_match(car_model, criteria)]
+    matches = [car_model for car_model in car_models
+               if criteria.year_interval.intersects(car_model.year_interval)]
     print("More filtering logic ...")
     return matches
 
 
-
-
 def apply_capacity_filter():
-    print(intervals_intersect(1000, 1600, 1250, 2000))
+    print(Interval(2000, 1600).intersects(Interval(1250, 2000)))
 
-
-def intervals_intersect(start1: int, end1: int, start2: int, end2: int):
-    return start1 <= end2 and start2 <= end1  # copiata cu grije din StackOverflow
 
 
 class Alta:
     def apply_capacity_filter(self):
-        print(intervals_intersect(1000, 1600, 1250, 2000))
+        interval_ = Interval(1000, 1600)
+        interval_1 = Interval(1250, 2000)
+        print(interval_.intersects(interval_1))
 
 
 class CarModelMapper:
-    def to_dto(self, car_model):
+    def to_dto(self, car_model:CarModel):
         dto = CarModelDTO()
         dto.make = car_model.get_make()
         dto.model = car_model.get_model()
-        dto.start_year = car_model.get_start_year()
+        # dto.start_year = car_model.get_start_year()
+        dto.start_year = car_model.year_interval.start
         dto.end_year = car_model.get_end_year()
         return dto
 
--- play.py
+++ play.py
@@ -1,9 +1,10 @@
+import re
 from dataclasses import dataclass
+from enum import Enum
 
 
 @dataclass(frozen=True)
 class A:
-
     name: str
 
     list: list[str]
@@ -15,19 +16,19 @@ class A:
         self.list.append("AAA")
 
 
-
 if __name__ == '__main__':
-    a = A("name",["a","b"])
+    a = A("name", ["a", "b"])
+
     # a.name = "x"
     a.list.append("AAA")
 
     print(a)
 
-
-x=True
-coll=[]
+x = True
+coll = []
 if (x and coll):
     print("x and coll")
+
 
 # @dataclass(unsafe_hash=True)
 class X:
@@ -35,7 +36,41 @@ class X:
     def __init__(self, a):
         self.a = a
 
+
 set = set()
 set.add(X(1))
 set.add(X(1))
 print(set)
+
+
+# typealias IP = str
+# from typing import TypeAlias
+# CouponId: TypeAlias = str
+# CustomerId: TypeAlias = str
+
+
+class CouponId(str):
+    pass
+
+
+class CustomerId(str):
+    pass
+
+
+def foo(coupon: CouponId, c: CustomerId):
+    pass
+
+
+foo(CouponId("1"), "2")
+
+
+map: dict[int,list[int]] = {}
+
+
+
+print(re.search(r"hostname: '(.+)'", "blabla"))
+
+class A(Enum):
+    A =1
+    B,
+    C,
