c552be30b59e75856528480a0c09f3072251fa39
Victor Rentea <victorrentea@gmail.com>
2025-03-17 16:56:40 +0200

    undo

--- boolean_parameters.py
+++ boolean_parameters.py
@@ -1,6 +1,3 @@
-from typing import Callable
-
-
 def blue_method(id, task):
     big_ugly_method(id, task)
 
@@ -19,15 +16,15 @@ def red_method(id, task):
 
 def use_case_323(id, task):
     # TODO The shared called method must execute logic specific for my use-case #323
-    big_ugly_method(id, task, lambda x: print("My Logic: ", x))
+    big_ugly_method(id, task)
 
 
-def big_ugly_method(store_id, task, callback: Callable = lambda x: {}):
+def big_ugly_method(store_id, task):
     print("Donkey Logic 1 ", task, " and ", store_id)
     print(task)
     print("Donkey Logic 3 ", task)
 
-    callback(task)
+    # print("Logic just for CR#323 : ", task)
 
     print("Sheep Logic 1 ", store_id)
     print("Sheep Logic 2 ", store_id)
--- car_search.py
+++ car_search.py
@@ -1,28 +1,14 @@
-from dataclasses import dataclass
-@dataclass(frozen=True)
-class Interval:
-    start: int
-    end: int
-
-    def __post_init__(self):
-        if self.start > self.end:
-            raise ValueError("Start year is larger than end year.")
-        #multe = safety: fail fast de cate ori poti, cat mai devreme
-        #putin = mai reusable
-
-    #pot si eu OOP: keep behavior close to data
-    def intersects(self, other):
-        return self.start <= other.end and other.start <= self.end
+def intervals_intersect(start1, end1, start2, end2):
+    return start1 <= end2 and start2 <= end1
 
 
 class CarSearchCriteria:
-    def __init__(self, interval: Interval, make):
+    def __init__(self, start_year, end_year, make):
         self.make = make
-        # if start_year > end_year:
-        #     raise ValueError("Start year is larger than end year.")
-        # self.start_year = start_year
-        # self.end_year = end_year
-        self.year_interval = interval
+        if start_year > end_year:
+            raise ValueError("Start year is larger than end year.")
+        self.start_year = start_year
+        self.end_year = end_year
 
     def get_start_year(self):
         return self.start_year
@@ -33,16 +19,15 @@ class CarSearchCriteria:
     def get_make(self):
         return self.make
 
-    @property
-    def year_interval(self):
-        return Interval(self.start_year, self.end_year)
-
 
 class CarModel:
-    def __init__(self, make, model, interval: Interval):
+    def __init__(self, make, model, start_year, end_year):
         self.make = make
         self.model = model
-        self.year_interval = interval
+        if start_year > end_year:
+            raise ValueError("Start year is larger than end year.")
+        self.start_year = start_year
+        self.end_year = end_year
 
     def get_end_year(self):
         self.make = 2
@@ -50,7 +35,6 @@ class CarModel:
 
     def get_start_year(self):
         return self.start_year
-
 
     def get_make(self):
         return self.make
@@ -61,35 +45,34 @@ class CarModel:
     def __str__(self):
         return f"CarModel{{make='{self.make}', model='{self.model}'}}"
 
+    def filter_car_models(criteria, car_models):
+        matches = [car_model for car_model in car_models
+                   if intervals_intersect(
+                criteria.get_start_year(), criteria.get_end_year(),
+                car_model.get_start_year(), car_model.get_end_year())]
+        print("More filtering logic ...")
+        return matches
 
-def filter_car_models(criteria: CarSearchCriteria, car_models: list[CarModel]):
-    matches = [car_model for car_model in car_models
-               if criteria.year_interval.intersects(car_model.year_interval)]
-    print("More filtering logic ...")
-    return matches
-
-
-def apply_capacity_filter():
-    print(Interval(2000, 1600).intersects(Interval(1250, 2000)))
-
+    def apply_capacity_filter():
+        print(intervals_intersect(1000, 1600, 1250, 2000))
 
 
 class Alta:
     def apply_capacity_filter(self):
-        interval_ = Interval(1000, 1600)
-        interval_1 = Interval(1250, 2000)
-        print(interval_.intersects(interval_1))
+        print(intervals_intersect(1000, 1600, 1250, 2000))
 
 
 class CarModelMapper:
-    def to_dto(self, car_model:CarModel):
+    def to_dto(self, car_model):
         dto = CarModelDTO()
         dto.make = car_model.get_make()
         dto.model = car_model.get_model()
-        # dto.start_year = car_model.get_start_year()
-        dto.start_year = car_model.year_interval.start
+        dto.start_year = car_model.get_start_year()
         dto.end_year = car_model.get_end_year()
         return dto
+
+    def from_dto(self, dto):
+        return CarModel(dto.make, dto.model, dto.start_year, dto.end_year)
 
 
 class CarModelDTO:
