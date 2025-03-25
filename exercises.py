d3da3d0a12d0e1b1852be13dcaa7f9184fdeb259
Victor Rentea <victorrentea@gmail.com>
2025-03-25 13:53:21 +0200

    undo

--- exercises.py
+++ exercises.py
@@ -1,60 +1,41 @@
 import random
 import unittest
 
-
 def odds(list):
     odds = []
-    for n in list:
-        if n % 2 != 0:
-            odds.append(n)
+
     print(odds)
     return odds
 
 def even_squared(list):
     even_squared = []
-    for n in list:
-        if n % 2 == 0:
-            even_squared.append(n**2)
+
     print(even_squared)
     return even_squared
 
 def indexed_strings(list):
     indexed = []
-    for index,n in enumerate(list):
-        indexed.append(f"{index}->{n}")
+
     print(indexed)
     return indexed
 
 def sum_odds(list):
     sum_odds = 0
-    for n in list:
-        if n % 2 != 0:
-            sum_odds += n
+
     print(sum_odds)
     return sum_odds
 
 def squared_by_index(list):
     dict = {}
-    for index,n in enumerate(list):
-        dict[index] = n**2
+
     print(dict)
     return dict
 
-# "fizz" if |3, "buzz" if |5, "fizz buzz" if |15
+# "fizz" if n|3, "buzz" if n|5, "fizz buzz" if n|15, n as string otherwise
 def fizz_buzz(n):
     fizz_buzz=[]
-    for n in range(1, n+1):
-        if n % 3 == 0 and n % 5 == 0:
-            fizz_buzz.append("fizz buzz")
-        elif n % 3 == 0:
-            fizz_buzz.append("fizz")
-        elif n % 5 == 0:
-            fizz_buzz.append("buzz")
-        else:
-            fizz_buzz.append(f"{n}")
+
     return fizz_buzz
-
-
 
 class Tests(unittest.TestCase):
 
