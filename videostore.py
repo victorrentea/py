b58ad7ea4a9b3e9eed38c4c58e9c4abeb5b7df61
Victor Rentea <victorrentea@gmail.com>
2025-04-01 09:54:42 +0300

    video store kata

--- videostore.py
+++ videostore.py
@@ -56,3 +56,26 @@ class Customer:
         result += "Amount owed is " + str(total_amount) + "\n"
         result += "You earned " + str(frequent_rents_points) + " frequent renter points"
         return result
+
+
+# ------ tests --------
+import unittest
+class CustomerTest(unittest.TestCase):
+    def test_something(self):
+        customer = Customer("John Doe")
+        customer.add_rental(Movie("Star Wars", Movie.NEW_RELEASE), 6)
+        customer.add_rental(Movie("Sofia", Movie.CHILDRENS), 7)
+        customer.add_rental(Movie("Inception", Movie.REGULAR), 5)
+
+        expected = "Rental Record for John Doe\n" \
+                   + "\tStar Wars\t18.0\n" \
+                   + "\tSofia\t7.5\n" \
+                   + "\tInception\t6.5\n" \
+                   + "Amount owed is 32.0\n" \
+                   + "You earned 4 frequent renter points"
+
+        self.assertEqual(expected, customer.statement())
+
+
+if __name__ == '__main__':
+    unittest.main()
