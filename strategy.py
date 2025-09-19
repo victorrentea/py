cd242fd49fa342fdf90d17c21fb9896b1b6368d2
Victor Rentea <victorrentea@gmail.com>
2025-09-19 11:27:18 +0300

    am spart elif/match-case mari in metode separate
    apoi in fisiere separate ca metode statice

--- strategy.py
+++ strategy.py
@@ -13,13 +13,33 @@ class Parcel:
 def calculate_customs_tax(parcel: Parcel) -> float:
     match parcel.origin_country:
         case "UK":
-            return parcel.tobacco_value / 2 + parcel.regular_value
+            return UKCustomsTaxCalculator.calculate(parcel)
         case "CN":
-            return parcel.tobacco_value + parcel.regular_value
+            return ChinaCustomsTaxCalculator.calculate(parcel)
         case "FR" | "ES" | "RO":
-            return parcel.tobacco_value / 3
+            return EUCustomsTaxCalculator.calculate(parcel)
         case _:
             raise ValueError(f"Not a valid country ISO2 code: {parcel.origin_country}")
+
+
+class EUCustomsTaxCalculator:
+    @staticmethod
+    def calculate(parcel: Parcel) -> float:
+        return parcel.tobacco_value / 3
+
+
+class ChinaCustomsTaxCalculator:
+    @staticmethod
+    def calculate(parcel: Parcel) -> float:
+        # +50 linie
+        return parcel.tobacco_value + parcel.regular_value
+
+
+class UKCustomsTaxCalculator:
+    @staticmethod
+    def calculate(parcel: Parcel) -> float:
+        # 100+ linii in 7 metode
+        return parcel.tobacco_value / 2 + parcel.regular_value
 
 
 if __name__ == "__main__":
